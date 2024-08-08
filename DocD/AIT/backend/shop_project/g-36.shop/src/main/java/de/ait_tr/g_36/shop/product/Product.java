e9eebf703535a9060657ad08f8f3924e4da39eba
Boris Iurciuc <boris.iurciuc@gmail.com>
2024-08-08 20:45:36 +0200

    first commit, tables product, customer

---
+++ DocD/AIT/backend/shop_project/g-36.shop/src/main/java/de/ait_tr/g_36/shop/product/Product.java
@@ -0,0 +1,69 @@
+package de.ait_tr.g_36.shop.product;
+
+import java.math.BigDecimal;
+import java.util.Objects;
+
+/**
+ * 07/08/2024 g-36.shop * @author Boris Iurciuc (cohort36)
+ */
+
+public class Product {
+  private Long id;
+  private String title;
+  private BigDecimal price;
+  private boolean active;
+
+  public Long getId() {
+    return id;
+  }
+
+  public void setId(Long id) {
+    this.id = id;
+  }
+
+  public String getTitle() {
+    return title;
+  }
+
+  public void setTitle(String title) {
+    this.title = title;
+  }
+
+  public BigDecimal getPrice() {
+    return price;
+  }
+
+  public void setPrice(BigDecimal price) {
+    this.price = price;
+  }
+
+  public boolean isActive() {
+    return active;
+  }
+
+  public void setActive(boolean active) {
+    this.active = active;
+  }
+
+  @Override
+  public boolean equals(Object o) {
+    if (this == o) {
+      return true;
+    }
+    if (!(o instanceof Product product)) {
+      return false;
+    }
+    return Objects.equals(getId(), product.getId());
+  }
+
+  @Override
+  public int hashCode() {
+    return Objects.hashCode(getId());
+  }
+
+  @Override
+  public String toString() {
+    return String.format("Product: id - %d, title - %s, price - %s, active - %s",
+        id, title, price, active ? "yes" : "no");
+  }
+}
